2af609c40fc48efb63ed0db8088144ee0e817452
Joscha von Andrian <joschavonandrian@gmail.com>
2022-12-15 23:49:34 +0100

    basic calendar

---
+++ src/components/calendar.tsx
@@ -0,0 +1,47 @@
+/* Calendar.tsx component */
+
+import { NextPage } from "next";
+import Calendar from '@toast-ui/react-calendar';
+import '@toast-ui/calendar/dist/toastui-calendar.min.css';
+import { useEffect } from "react";
+
+
+interface NewEvent{
+  id: string,
+      calendarId: string,
+      title: string,
+      start: string,
+      end: string,
+      category: string,
+}
+
+interface Props {
+  events: NewEvent[],
+  calendars: string[]
+}
+
+
+const TimeCalendar: NextPage<Props> = (props) => {
+  const { events, calendars } = props;
+  useEffect(() => {
+  console.log(events);
+  }, [])
+    const newCalendars = calendars.map((calendar) => {
+        var randomColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');;
+        return { id: calendar, name: calendar ,backgroundColor: randomColor,}
+    })
+  
+   return (
+    <div className="w-full">
+      <Calendar 
+      usageStatistics={false}
+      isReadOnly={true}
+      calendars={newCalendars}
+      events={events}
+      />
+    </div>
+  );
+};
+
+export default TimeCalendar;
+
