1f8d6610f1c99b40fc2d997f3b671543c1ae4ee8
Joscha von Andrian <joschavonandrian@gmail.com>
2022-12-16 00:21:13 +0100

    prettier files

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,49 +1,46 @@
 /* Calendar.tsx component */
 
 import { NextPage } from "next";
-import Calendar from '@toast-ui/react-calendar';
-import '@toast-ui/calendar/dist/toastui-calendar.min.css';
+import Calendar from "@toast-ui/react-calendar";
+import "@toast-ui/calendar/dist/toastui-calendar.min.css";
 import { useEffect } from "react";
 
-
-interface NewEvent{
-  id: string,
-      calendarId: string,
-      title: string,
-      start: string,
-      end: string,
-      category: string,
+interface NewEvent {
+  id: string;
+  calendarId: string;
+  title: string;
+  start: string;
+  end: string;
+  category: string;
 }
 
 interface Props {
-  events: NewEvent[],
-  calendars: string[]
+  events: NewEvent[];
+  calendars: string[];
 }
-
 
 const TimeCalendar: NextPage<Props> = (props) => {
   const { events, calendars } = props;
   useEffect(() => {
-  console.log(events);
-  }, [])
-    const newCalendars = calendars.map((calendar) => {
-        var randomColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');;
-        return { id: calendar, name: calendar ,backgroundColor: randomColor,}
-    })
-  
-   return (
+    console.log(events);
+  }, []);
+  const newCalendars = calendars.map((calendar) => {
+    var randomColor =
+      "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+    return { id: calendar, name: calendar, backgroundColor: randomColor };
+  });
+
+  return (
     <div className="w-full">
-      <Calendar 
-      usageStatistics={false}
-      week={{eventView: ['time'],
-taskView:false}}
-      isReadOnly={true}
-      calendars={newCalendars}
-      events={events}
+      <Calendar
+        usageStatistics={false}
+        week={{ eventView: ["time"], taskView: false }}
+        isReadOnly={true}
+        calendars={newCalendars}
+        events={events}
       />
     </div>
   );
 };
 
 export default TimeCalendar;
-
