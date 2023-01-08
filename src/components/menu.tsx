e549f48e91f443d3c03eceafaee3511d24b01c23
Joscha von Andrian <joschavonandrian@gmail.com>
2023-01-08 18:06:03 +0100

    add title

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,4 +1,6 @@
-import { Calendar, Setting3, TableDocument } from "iconsax-react";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Calendar, Link2, Setting3, TableDocument } from "iconsax-react";
 
 interface MenuProps {
   onClick: React.Dispatch<React.SetStateAction<number>>;
@@ -8,7 +10,11 @@ const Menu = ({ onClick }: MenuProps) => {
   return (
     <div className="drawer-side">
       <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+
       <ul className="menu w-80 bg-base-100 p-4 text-base-content">
+        <h1 className="text-center font-bold">ClickUp</h1>
+        <h4 className="text-center">Time Track</h4>
+        <div className="divider"></div>
         <li>
           <button onClick={() => onClick(0)}>
             <Calendar size="32" />
@@ -26,6 +32,18 @@ const Menu = ({ onClick }: MenuProps) => {
             <Setting3 size="32" />
             Settings
           </button>
+        </li>
+        <li className="menu-title">
+          <span>Links</span>
+        </li>
+        <li>
+          <a
+            href="https://github.com/joscha0/clickup-time-track"
+            target="_blank"
+          >
+            <Link2 size="32" />
+            Github
+          </a>
         </li>
       </ul>
     </div>
