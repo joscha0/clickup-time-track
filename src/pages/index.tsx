d9a71f931f3bb476277c8a86fc54be894114f78f
Joscha von Andrian <joschavonandrian@gmail.com>
2023-01-09 23:50:16 +0100

    add day of the week

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import {
   Typography,
   useTheme,
 } from "@mui/material";
+import { ResponsiveBar } from "@nivo/bar";
 import { ResponsiveCalendar } from "@nivo/calendar";
 import type { NextPage } from "next";
 import Head from "next/head";
@@ -18,6 +19,7 @@ const Home: NextPage = () => {
   const [events, setEvents] = useState<NewEvent[]>([]);
 
   const [calendarEntries, setCalendarEntries] = useState<CalendarEntry[]>([]);
+  const [dayOfWeekEntries, setDayOfWeekEntries] = useState<number[]>([]);
 
   const [isLoading, setLoading] = useState(false);
 
@@ -39,6 +41,8 @@ const Home: NextPage = () => {
         setEvents(newEvents);
 
         const calendarData: CalendarEntry[] = [];
+        const dayOfWeekData: number[] = [0, 0, 0, 0, 0, 0, 0];
+
         newEvents.forEach((event: NewEvent) => {
           const day = new Date(event.start).toISOString().split("T")[0];
           const calendarItemIndex = calendarData.findIndex(
@@ -49,8 +53,11 @@ const Home: NextPage = () => {
           } else {
             calendarData[calendarItemIndex]!.value += +event.duration;
           }
+
+          dayOfWeekData[new Date(event.start).getDay()] += +event.duration;
         });
         setCalendarEntries(calendarData);
+        setDayOfWeekEntries(dayOfWeekData);
       }
       setLoading(false);
     });
@@ -97,7 +104,7 @@ const Home: NextPage = () => {
                       from={new Date(new Date().getFullYear() - 1, 1, 1)}
                       to={new Date(new Date().getFullYear(), 11, 31)}
                       emptyColor={theme.palette.background.paper}
-                      colors={["#61cdbb", "#97e3d5", "#e8c1a0", "#f47560"]}
+                      colors={["#122B56", "#1A3C77", "#2D60B8", "#5E98FD"]}
                       margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
                       yearSpacing={40}
                       monthBorderColor={theme.palette.action.disabledBackground}
@@ -129,6 +136,69 @@ const Home: NextPage = () => {
                         },
                       ]}
                     />
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={1}>
+                <Card>
+                  <CardContent sx={{ height: 500 }}>
+                    <Typography textAlign="center" variant="h5">
+                      Day of the Week
+                    </Typography>
+                    <Box sx={{ p: 5, height: 450 }}>
+                      <ResponsiveBar
+                        theme={{
+                          textColor: theme.palette.text.primary,
+                          axis: {
+                            ticks: {
+                              text: {
+                                fontSize: theme.typography.body1.fontSize,
+                              },
+                            },
+                          },
+                        }}
+                        margin={{ bottom: 50 }}
+                        axisBottom={{
+                          tickSize: 5,
+                          tickPadding: 5,
+                          tickRotation: 0,
+                        }}
+                        enableLabel={false}
+                        axisLeft={null}
+                        isInteractive={false}
+                        colors={["#3373E3"]}
+                        data={[
+                          {
+                            id: "Mon",
+                            value: dayOfWeekEntries[0]?.toString() ?? "0",
+                          },
+                          {
+                            id: "Tue",
+                            value: dayOfWeekEntries[1]?.toString() ?? "0",
+                          },
+                          {
+                            id: "Wed",
+                            value: dayOfWeekEntries[2]?.toString() ?? "0",
+                          },
+                          {
+                            id: "Thu",
+                            value: dayOfWeekEntries[3]?.toString() ?? "0",
+                          },
+                          {
+                            id: "Fri",
+                            value: dayOfWeekEntries[4]?.toString() ?? "0",
+                          },
+                          {
+                            id: "Sat",
+                            value: dayOfWeekEntries[5]?.toString() ?? "0",
+                          },
+                          {
+                            id: "Sun",
+                            value: dayOfWeekEntries[6]?.toString() ?? "0",
+                          },
+                        ]}
+                      />
+                    </Box>
                   </CardContent>
                 </Card>
               </Grid>
