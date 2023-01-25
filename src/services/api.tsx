7b596964a6098b11274de0021e9a5086b0a8e802
Joscha von Andrian <joschavonandrian@gmail.com>
2023-01-25 19:32:36 +0100

    fix task undefined

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -8,6 +8,64 @@ export interface NewEvent {
   duration: string;
 }
 
+export interface ApiElement {
+  id: string;
+  task: ApiTask;
+  wid: string;
+  user: ApiUser;
+  billable: boolean;
+  start: string;
+  end: string;
+  duration: string;
+  description: string;
+  tags: any[];
+  source: string;
+  at: string;
+  task_location: ApiTaskLocation;
+  task_tags: ApiTaskTag[];
+  task_url: string;
+}
+
+export interface ApiTask {
+  id: string;
+  custom_id: string;
+  name: string;
+  status: ApiStatus;
+  custom_type: null;
+}
+
+export interface ApiStatus {
+  status: string;
+  color: string;
+  type: string;
+  orderindex: number;
+}
+
+export interface ApiTaskLocation {
+  list_id: string;
+  folder_id: string;
+  space_id: string;
+  list_name: string;
+  folder_name: string;
+  space_name: string;
+}
+
+export interface ApiTaskTag {
+  name: string;
+  tag_fg: string;
+  tag_bg: string;
+  creator: number;
+}
+
+export interface ApiUser {
+  id: number;
+  username: string;
+  email: string;
+  color: string;
+  initials: string;
+  profilePicture: string;
+}
+
 export const getEventsData = async (apiKeyReq: string, teamIdReq: string) => {
   if (apiKeyReq == "" || teamIdReq == "") {
     // navigate login
@@ -28,19 +86,26 @@ export const getEventsData = async (apiKeyReq: string, teamIdReq: string) => {
     if (data.data !== undefined) {
       const calendars: string[] = [];
       const newEvents: NewEvent[] = [];
-      data.data.forEach((element: any) => {
-        if (!calendars.includes(element.task_location.space_id)) {
-          calendars.push(element.task_location.space_id);
+      data.data.forEach((element: ApiElement) => {
+        try {
+          if (!calendars.includes(element.task_location.space_id)) {
+            calendars.push(element.task_location.space_id);
+          }
+          newEvents.push({
+            id: element.id,
+            calendarId:
+              element.task_location === undefined
+                ? ""
+                : element.task_location.space_id,
+            title: element.task === undefined ? "" : element.task.name,
+            start: new Date(parseInt(element.start)).toISOString(),
+            end: new Date(parseInt(element.end)).toISOString(),
+            category: "time",
+            duration: element.duration,
+          });
+        } catch (error) {
+          console.error(error);
         }
-        newEvents.push({
-          id: element.id,
-          calendarId: element.task_location.space_id,
-          title: element.task.name,
-          start: new Date(parseInt(element.start)).toISOString(),
-          end: new Date(parseInt(element.end)).toISOString(),
-          category: "time",
-          duration: element.duration,
-        });
       });
       newEvents.reverse();
       return { newEvents, calendars };
